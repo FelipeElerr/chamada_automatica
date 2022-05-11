21e2f445a7203069a15485fb558f7d4356c5929a
liperubo <liperubo@gmail.com>
2022-05-11 15:28:53 -0300

    QRCode funcional

--- WEB/chamada_automatica/src/App.js
+++ WEB/chamada_automatica/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import { QRCode } from 'react-qrcode-logo';
+import * as React from 'react'
+
+const url = "https://api.hashify.net/hash/sha256/hex?value="
+const code = "CP500TIN2-03052022-1900"
+var hashed = "1"
 
 function App() {
+  const [hash, setHash] = React.useState(); 
+
+  const funcaoAssync = async () => {
+    const string = url.concat(code)
+    const resultado = await fetch(string);
+    const json = await resultado.json();
+
+    //JSON.parse(document.getElementById("a").innerHTML).Digest
+    setHash(json.Digest)
+	};
+  React.useEffect(() => { funcaoAssync(); }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <QRCode value={hash}/>
     </div>
   );
 }
