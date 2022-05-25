c77aa50c0a7c7d5b523daa72c60fc808431321df
Felipe Eler <200738@facens.br>
2022-05-24 22:20:17 -0300

    Adicionando tela de início de chamada e ajustando componentes header e footer

--- WEB/chamada_automatica/src/pages/Main/main.js
+++ WEB/chamada_automatica/src/pages/Main/main.js
@@ -1,10 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import './main.css'
+import Header from '../../componentes/Header/header'
+import Footer from '../../componentes/Footer/footer'
 
-function Main(){
-    return(
-        <h1>
-            Página incial 
-        </h1>
+
+function Main() {
+    // const trocarPagina = values =>{
+    //     history.goForward('/nova-chamada')
+    // }
+    return (
+        <>
+            <Header />
+            <article className="opcaoChamada" >
+                <section className="novaChamada" id="formatacao">
+                    <p>
+                        Nova chamada
+                    </p>
+                </section>
+                <section className="historicoChamada" id="formatacao">
+                    <p>
+                        Histórico de chamadas
+                    </p>
+                </section>
+            </article>
+            <article className="telaListaAlunos">
+                <section className="sala" id="formatacao">
+                    <p>
+                        UP013TIN1
+                    </p>
+                </section>
+
+                <section className="listaAlunos" id="formatacao" >
+                    <ul >
+                        <li>Alex Shoiti</li>
+                        <li>Felipe Eler</li>
+                        <li>Felipe Victorino</li>
+                        <li>Gabriel Mancio</li>
+                        <li>Laura Cecília</li>
+                        <li>Matheus Patriarca</li>
+                        <li>Rafael Lavagnini</li>
+                    </ul>
+                </section>
+
+                <section className="gerarQrCode">
+                    <Link to="/nova-chamada" className="Link" id="formatacao">Gerar Qr Code</Link>
+                </section>
+
+            </article>
+            <Footer />
+        </>
+
     )
 }
 
--- WEB/chamada_automatica/src/routes.js
+++ WEB/chamada_automatica/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from "./pages/Main/main";
 import NovaChamada from "./pages/NovaChamada/nova-chamada";
 
+
 function Router() {
     return (
         <BrowserRouter>
