ba86fb0385bbf7dc56958cccef6e0e9dc2782ee9
liperubo <liperubo@gmail.com>
2022-06-05 20:33:51 -0300

    adicionando a leitura de QRcode ao projeto

---
+++ WEB/chamada_automatica/src/pages/Aluno/QRCodeReader.js
@@ -0,0 +1,90 @@
+import { Html5Qrcode } from "html5-qrcode"
+import React from "react";
+import axios from 'axios'
+
+//const url = 'http://localhost:3080'
+const url = 'https://6cd5-2804-18-185c-ff00-41e-6878-a537-6bb8.sa.ngrok.io'
+
+function QRCodeReader() {
+  function callApi(ra, decodedText){
+    axios.post(`${url}/cadastraaluno`,{
+      	"ra": ra,
+      	"qrcode": decodedText
+      })
+    .then((response)=>{
+      //window.alert("foi")
+    })
+  }
+  // function testeGET(){
+  //   axios.get(`${url}/`)
+  //   .then((response)=>{
+  //     console.log(response)
+  //   })
+  // }
+  // function testePOST(){
+  //   let email = document.getElementById("email").value
+  //   var array = email.split("@")
+  //   let ra = array[0]
+
+  //   axios.post(`${url}/cadastraaluno`,{
+  //     	"ra": ra,
+  //     	"qrcode": "decodedText"
+  //     })
+  //   .then((response)=>{
+  //     console.log(response)
+  //   })
+  // }
+	function scan(){
+		Html5Qrcode.getCameras().then(devices => {
+			var cameraId = 0
+			if (devices && devices.length) {
+				devices.forEach(device => {
+					var string = device.label.toString()
+					var array = string.split(", ")
+          if(array[1] === "facing back"){
+						cameraId = device.id;
+					}
+				});
+			
+				const html5QrCode = new Html5Qrcode("reader");
+				html5QrCode.start(
+					cameraId, 
+					{
+						fps: 10,
+						qrbox: { width: 250, height: 250 }
+					},
+					(decodedText, decodedResult) => {
+						let email = document.getElementById("email").value
+            var array = email.split("@")
+            let ra = array[0]
+						console.log(`${email} -> ${ra}, com o código ${decodedText}`);
+
+            callApi(ra, decodedText)
+
+            html5QrCode.stop().then((ignore) => {
+              // QR Code scanning is stopped.
+            }).catch((err) => {
+              // Stop failed, handle it.
+            });
+					},
+					(errorMessage) => {
+						console.warn(`Code scan error = ${errorMessage}`);
+					})
+				.catch((err) => {
+					console.error(`Code scan error = ${err}`);
+				});
+			}
+		}).catch(err => {
+			console.error(`Camera error = ${err}`);
+		});
+	}
+  return (
+    <div style={{width: "500px", border: "white 2px"}} id="reader">
+			<button onClick={scan}>Escanear QRCode</button>
+      {/* <button onClick={testeGET}>Testar GET</button> */}
+      {/* <button onClick={testePOST}>Testar POST</button> */}
+		</div>
+  );
+}
+
+export default QRCodeReader;
--- WEB/chamada_automatica/src/pages/Aluno/alunos.js
+++ WEB/chamada_automatica/src/pages/Aluno/alunos.js
@@ -1,8 +1,17 @@
 import React from "react";
+import QRCodeReader from './QRCodeReader';
 
 export default function Alunos(){
-    return(
-        <>
-        </>
-    )
+	return(
+	<div className="App">
+		<header className="App-header">
+				<label htmlFor="email">E-mail</label>
+				<input type="email" name="email" id="email" />
+
+			<QRCodeReader />
+				{/* <p>Usuário: {user?.login}</p>
+				<p>Biografia: {user?.bio}</p> */}
+		</header>
+	</div>
+	)
 }
