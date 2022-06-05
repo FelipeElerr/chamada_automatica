d8f9d96d352a47dc9ae58f066be586dab0c700be
Felipe Eler <200738@facens.br>
2022-06-05 20:26:44 -0300

    Adicionando API do Rafael

---
+++ API/src/routes/alunosRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+import AlunoController from "../controllers/alunosController.js";
+
+const router = express.Router();
+
+router
+    .get("/alunos", AlunoController.listarAlunos)
+    .post("/alunos", AlunoController.cadastrarAluno)
+    .put("/alunos/presenca", AlunoController.presenca)
+    .put("/alunos/presenca/geral", AlunoController.presencaGeral)
+    .delete("/alunos/:ra", AlunoController.excluirAluno)
+
+export default router;
