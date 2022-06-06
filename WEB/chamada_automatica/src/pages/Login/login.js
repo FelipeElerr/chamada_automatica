6319f7e09ceddb7a2fdc87052f1c18376f7b1c3d
Felipe Eler <200738@facens.br>
2022-06-05 22:46:37 -0300

    Melhorando navegação de páginas

--- WEB/chamada_automatica/src/pages/Login/login.js
+++ WEB/chamada_automatica/src/pages/Login/login.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Footer from "../../componentes/Footer/footer";
 import { Link } from "react-router-dom";
+import './login.css'
 
-export default function Login(){
-    return(
+export default function Login() {
+    return (
         <>
-         
-             <Link to="/alunos">Aluno</Link>
-             <Link to="/alunos-presentes">Professor</Link>
+            <div className="centro">
 
-        
-        <Footer />
+                <section className="aluno">
+                    <Link to="/alunos" className="Link" id="formatacao">Aluno</Link>
+                </section>
+                <section className="professor">
+                    <Link to="/conferencia-alunos" className="Link" id="formatacao">Professsor</Link>
+                </section>
+
+            </div>
+            <Footer />
         </>
     )
 }
